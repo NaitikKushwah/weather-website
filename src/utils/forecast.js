2349c94e08977b65f3115e6e10c5638896981f61
NaitikKushwah <naitik.kushwah9@gmail.com>
2022-06-07 16:00:33 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=d2db9e4a5342ccb2b9747532df5441d2&query=${latitude},${longitude}`;
+    request({ url, json : true}, (error, { body }) => {
+
+    if(error){
+        callback('Unable to connect to weather application', undefined);
+    }else if(body.error){
+        callback('Unable to find location, please enter again', undefined);
+    }else{
+        const temperature = body.current.temperature;
+        const feelslike = body.current.feelslike;
+
+        callback(undefined,`It is currently ${temperature}, It feels like ${feelslike} degrees out.`);
+        } 
+    });
+}
+module.exports = forecast;
+
